4555de66290e0dac7ccb7dd6cb507ca95c301ea0
Rob Conery <rob@conery.io>
2025-05-21 10:33:34 -1000

    More attempts to fix

--- server/api/stripe-receiver.post.ts
+++ server/api/stripe-receiver.post.ts
@@ -3,7 +3,8 @@ import { Order } from '../models'
 import { defineEventHandler, readBody, createError, getHeader, readRawBody } from 'h3'
 import Stripe from 'stripe'
 import { getCheckoutSession } from "../lib/stripe"
-
+import assert from "assert"
+import { sign } from "crypto"
 
 // Initialize Stripe with the API key
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
@@ -16,13 +17,16 @@ export default defineEventHandler(async (event) => {
   const body = await readRawBody(event)
   const signature = getHeader(event, 'stripe-signature')
   const out = {
-    statusCode: 400,
     statusMessage: 'Invalid request'
   }
-  if (!signature) {
-    out.statusMessage = 'Missing stripe-signature header'
-    return out;
-  }
+
+  assert(process.env.STRIPE_WEBHOOK_SECRET, 'Missing STRIPE_WEBHOOK_SECRET in environment variables')
+  assert(signature, 'Missing stripe-signature header');
+
+  // if (!signature) {
+  //   out.statusMessage = 'Missing stripe-signature header'
+  //   return out;
+  // }
 
   let stripeEvent: Stripe.Event | undefined;
   
@@ -36,7 +40,7 @@ export default defineEventHandler(async (event) => {
   } catch (err: any) {
     console.error(`Webhook signature verification failed: ${err.message}`)
     out.statusMessage = `Webhook signature verification failed: ${err.message}`
-    out.statusCode = 401
+    setResponseStatus(event, 400)
     return out;
   }
 
@@ -45,20 +49,15 @@ export default defineEventHandler(async (event) => {
     try {
       //const session = stripeEvent.data.object as Stripe.Checkout.Session
       const session = getCheckoutSession(stripeEvent.data.object.id);
-      // Ensure we have a valid session with the necessary data
-      // if (!session.customer_email && !session.customer_details?.email || session.payment_status !== 'paid') {
-      //   out.statusMessage = 'Invalid or unpaid session data'
-      //   return out;
-      // }
       
       // The updated Order.saveStripeOrder method only needs the session parameter
       await Order.saveStripeOrder(session);
-      out.statusCode = 200;
+      setResponseStatus(event, 200);
       out.statusMessage = 'Order processed successfully'
 
     } catch (error: any) {
       console.error('Error processing checkout session:', error)
-      out.statusCode = 500
+      setResponseStatus(event, 500)
       out.statusMessage = `Failed to process checkout: ${error.message}`
       // Optionally, you can throw an error or return a specific response
     }
